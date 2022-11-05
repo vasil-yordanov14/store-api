fda40f2065db4449f47a65baebdd9252fe5e9f5c
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-05 14:45:23 +0200

    set-router

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -13,9 +14,10 @@ const errorMiddleware = require("./middleware/error-handler");
 app.use(express.json());
 
 // routes
+app.use("/api/v1/products", productsRouter);
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Store API</h1><a href="/api/v1/products>products route</a>`);
+  res.send(`<h1>Store API</h1><a href="/api/v1/products">products route</a>`);
 });
 
 // products route
