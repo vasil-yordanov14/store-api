28face2373d1e0d6249a59767c17b28ef5383b9c
vasil-yordanov14 <vasil_sh@abv.bg>
2022-11-05 22:05:16 +0200

    select-option

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,11 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("-name price");
+  const products = await Product.find({}).select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -25,6 +25,12 @@ const getAllProducts = async (req, res) => {
   } else {
     result = result.sort("createdAt");
   }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
